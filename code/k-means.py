850ac914376fc41d74b5187def2f232f2d986e13
Amit There <amitthere@users.noreply.github.com>
2018-10-10 19:06:31 -0400

    Added Euclidean distance methods

--- code/k-means.py
+++ code/k-means.py
@@ -22,8 +22,41 @@ class Kmeans:
     K-Means clustering implementation
     """
 
-    def __init__(self):
-        self.centroid = None
+    def __init__(self, data):
+        self.init_centroids = None
+        self.centroids = None
+        self.data = data
+
+    def initial_random_centroids(self, num):
+        """
+        Random centroids chosen from the input dataset
+        :param num:
+        :return centroid_indics:
+        """
+        centroid_indices = np.random.choice(self.data.shape[0], num)
+        self.init_centroids = self.data[centroid_indices]
+        self.centroids = self.init_centroids
+        return centroid_indices
+
+    def euclidean_distance(self, objSet, obj):
+        """Euclidean distance between set of objects and single object"""
+        return np.linalg.norm(objSet - obj, ord=2, axis=1)
+
+    def centroid_distance(self, centroid):
+        """ Euclidean distance implemented to calculate distance of each object in dataset from given centroid """
+        diff = self.data - centroid
+        square = np.square(diff)
+        sum = np.sum(square, axis=1)
+        sqrt = np.sqrt(sum)
+        return sqrt
+
+    def distance_from_centroids(self):
+        rows = self.centroids.shape[0]
+        cols = self.centroids.shape[0]
+        dist = np.zeros((rows, cols), dtype=float)
+
+        for centroid in self.centroids:
+            dist[0] = self.centroid_distance(centroid)
 
 
 
