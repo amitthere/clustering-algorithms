9f0858606d1372ccf08999b90a21d1633ce17d1b
Amit There <amitthere@users.noreply.github.com>
2018-10-10 19:37:12 -0400

    Added distance_from_centroids(). Changed datatype of imported file values to float

--- code/k-means.py
+++ code/k-means.py
@@ -14,7 +14,7 @@ class Import:
             self.import_tab_file(self.file)
 
     def import_tab_file(self, tabfile):
-        self.data = np.genfromtxt(tabfile, dtype=str, delimiter='\t')
+        self.data = np.genfromtxt(tabfile, dtype=float, delimiter='\t')
 
 
 class Kmeans:
@@ -26,12 +26,13 @@ class Kmeans:
         self.init_centroids = None
         self.centroids = None
         self.data = data
+        self.clusters = np.zeros((self.data, 1))
 
     def initial_random_centroids(self, num):
         """
         Random centroids chosen from the input dataset
         :param num:
-        :return centroid_indics:
+        :return centroid_indices:
         """
         centroid_indices = np.random.choice(self.data.shape[0], num)
         self.init_centroids = self.data[centroid_indices]
@@ -44,19 +45,17 @@ class Kmeans:
 
     def centroid_distance(self, centroid):
         """ Euclidean distance implemented to calculate distance of each object in dataset from given centroid """
-        diff = self.data - centroid
-        square = np.square(diff)
-        sum = np.sum(square, axis=1)
-        sqrt = np.sqrt(sum)
-        return sqrt
+        return np.sqrt(np.sum(np.square(self.data - centroid), axis=1))
 
     def distance_from_centroids(self):
-        rows = self.centroids.shape[0]
+        rows = self.data.shape[0]
         cols = self.centroids.shape[0]
-        dist = np.zeros((rows, cols), dtype=float)
+        distance_matrix = np.zeros((rows, cols), dtype=float)
 
-        for centroid in self.centroids:
-            dist[0] = self.centroid_distance(centroid)
+        for index, centroid in enumerate(self.centroids):
+            distance_matrix[:, index] = self.centroid_distance(centroid)
+
+        return distance_matrix
 
 
 
@@ -65,6 +64,9 @@ def main():
     dataset1 = Import(r'../data/cho.txt', 'TAB')
     dataset2 = Import(r'../data/iyer.txt', 'TAB')
 
+    km1 = Kmeans(dataset1.data[:, 2:])
+    ic1 = km1.initial_random_centroids(5)
+    km1.distance_from_centroids()
 
     return
 
