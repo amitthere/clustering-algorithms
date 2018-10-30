5cd1e8bedb76f2a2f792a4c2df96aa54adc43818
sandeshvp <sandeshv@buffalo.edu>
2018-10-30 01:42:49 -0400

    File import

--- code/dbscan.py
+++ code/dbscan.py
@@ -1,1 +1,21 @@
 import numpy as np
+
+class Import:
+    """
+    Imports data from various sources.
+    Currently, just tab-delimited files are supported
+    """
+
+    def __init__(self, file, ftype):
+        self.data = None
+        self.prefixed_data = None
+        self.file = file
+        if ftype == "TAB":
+            self.import_tab_file(self.file)
+
+    def import_tab_file(self, tabfile):
+        self.data = np.genfromtxt(tabfile, dtype = float, delimiter = '\t')
+
+
+def main():
+    file = Import('../data/cho.txt', "TAB")
