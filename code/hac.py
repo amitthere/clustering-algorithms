02db4cb2a0fc2d025f0a65a28044accd02fb476e
sandeshvp <sandeshv@buffalo.edu>
2018-10-30 01:31:23 -0400

    Finished

--- code/hac.py
+++ code/hac.py
@@ -2,6 +2,9 @@ import numpy as np
 from numpy import *
 from sklearn.metrics.pairwise import euclidean_distances
 import sys
+from sklearn.decomposition import PCA
+import matplotlib.pyplot as plt
+import seaborn as sb
 
 class Import:
     """
@@ -19,11 +22,12 @@ class Import:
     def import_tab_file(self, tabfile):
         self.data = np.genfromtxt(tabfile, dtype = float, delimiter = '\t')
 
-
+sys.setrecursionlimit(1500)
 file = Import('../data/cho.txt', "TAB")
-number_of_clusters = 3
+number_of_clusters = 5
 file0 = file.data
 gene_id = file0[:,0]
+final = len(gene_id)
 gene_ids = np.ravel(gene_id)
 cluster = []
 for i in range(len(gene_ids)):
@@ -61,27 +65,77 @@ def clusters(disMat, dis_r, dis_c, x, y, number_of_clusters):
             disMat[i][x] = disMat[i][y]
         if disMat[x][i] > disMat[y][i]:
             disMat[x][i] = disMat[y][i]
-        #disMat[i][x] = min(disMat[i][x], disMat[i][y])
-        #disMat[x][i] = min(disMat[x][i], disMat[y][i])
     disMat = np.delete(disMat, y, 0)
     disMat = np.delete(disMat, y, 1)
     if(disMat.shape != (number_of_clusters, number_of_clusters)):
         min_value(dis_r=dis_r, dis_c=dis_c, disMat=disMat, number_of_clusters=number_of_clusters)
     else:
+        print("Distance Matrix")
         print(disMat)
 
-def main():
+def rand_jaccard(labels, calculated):
 
-    #print(gene_ids)
+    labelMatrix = np.zeros((labels.shape[0], labels.shape[0]))
+    calculatedMatrix = np.zeros((labels.shape[0], labels.shape[0]))
+    for  i in range(labels.shape[0]):
+        for j in range(labels.shape[0]):
+            if(np.array_equal(labels[i], labels[j])):
+                labelMatrix[i][j] = 1
+            if(np.array_equal(calculated[i], calculated[j])):
+                calculatedMatrix[i][j] = 1
+    M11 = M00 = M10 = M01 = 0
+    for i in range(calculatedMatrix.shape[0]):
+        for j in range(calculatedMatrix.shape[0]):
+            if calculatedMatrix[i][j] == labelMatrix[i][j] == 1:
+                M11 = M11 + 1
+            elif calculatedMatrix[i][j] == labelMatrix[i][j] == 0:
+                M00 = M00 + 1
+            elif calculatedMatrix[i][j] == 1 and labelMatrix[i][j] == 0:
+                M10 = M10 + 1
+            elif calculatedMatrix[i][j] == 0 and labelMatrix[i][j] == 1:
+                M01 = M01 + 1
+    rand_index = float(M11 + M00)/float(M11 + M00 + M10 + M01)
+    jaccard_coefficient = float(M11)/float(M11 + M10 + M01)
+    print('Rand Coefficient = ', rand_index)
+    print('Jaccard Coefficient = ', jaccard_coefficient)
+
+
+def principal_component_analysis(data, labels):
+    pca_data = PCA(n_components=2).fit_transform(data)
+    plotPCA(pca_data, labels)
+
+def plotPCA(pcaComponents, labels):
+    x = pcaComponents[:, 0]
+    y = pcaComponents[:, 1]
+    fig = plt.figure()
+    scatter = sb.scatterplot(x, y, hue = labels)
+    plot = scatter.get_figure()
+    plt.xlabel('Component 1')
+    plt.ylabel('Component 2')
+    plt.title('Principal component analysis plot on Cho.txt with 5 clusters')
+    plt.legend()
+    plt.show()
+    plot.savefig('../Plots/HAC_CHO_PCA.png')
+
+def main():
     ground_truth_label = file0[:,1]
     gene_data = file0[:,2:]
     rows, columns = gene_data.shape
     #print(gene_data.shape)
-    truth_matrix = np.zeros((rows,rows))
     disMat = eucli_dis(gene_data)
-    print(disMat.shape)
+    #print(disMat.shape)
     dis_r, dis_c = disMat.shape
     min_value(dis_r, dis_c, disMat, number_of_clusters)
+    print(cluster)
+    list = np.zeros(len(gene_id), dtype=int)
+    for i in range(len(cluster)):
+        for j in cluster[i]:
+            list[j] = i
+    #print(list)
+    length = len(list)
+    #print(length)
+    rand_jaccard(ground_truth_label, list)
+    principal_component_analysis(gene_data, list)
 
 if __name__ == "__main__":
     main()
